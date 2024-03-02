a5fddff40653a659050200f30631ab997cd5f92f
Marquestt <thatyanecmarques98@live.com>
2024-03-02 19:20:44 -0300

    Adicionado verificação e informação se o número é maior ou menor

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    }
+    else {
+        if(chute > numeroSecreto) { //se o chute for maior que o número secreto
+            exibirTextoNaTela('h1', 'Ooops!');
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        }
+        else { //se o chute for menor que o número secreto
+            exibirTextoNaTela('h1', 'Ooops!');
+            exibirTextoNaTela('p', 'O número secreto é maior!');
+        }
+    }
 }
 
 function gerarNumeroAleatorio(){
